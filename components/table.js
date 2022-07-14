7c5db3bba83d117548c8da198b9e5b048dcd80a3
Marion Rose <marionrose@Marions-MacBook-Pro.local>
2022-07-14 12:17:17 -0500

    added watermelon component to clean up index and updated imageUrl stuff

--- components/table.js
+++ components/table.js
@@ -1,9 +1,38 @@
-import React, { PureComponent } from 'react'
+import { urlFor } from '../lib/sanity'
 
-export class Table extends PureComponent {
-  render() {
-    return <div>Table</div>
-  }
+export default function Table(carrot) {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Count</th>
+          <th>Purchase Date</th>
+          <th>Purchase Location</th>
+          <th>Brand</th>
+          <th>Variety</th>
+          <th>Image</th>
+          <th>Sweet Scale</th>
+          <th>Texture Scale</th>
+          <th>Rating</th>
+        </tr>
+      </thead>
+      <tbody>
+        {carrot.watermelons.map((watermelon, count) => (
+          <tr key={count}>
+            <td>{watermelon.count}</td>
+            <td>{watermelon.purchased}</td>
+            <td>{watermelon.purchaseLocation}</td>
+            <td>{watermelon.origin}</td>
+            <td>{watermelon.variety}</td>
+            <td>
+              <img src={urlFor(watermelon.image)} />
+            </td>
+            <td>{watermelon.sweetScale}</td>
+            <td>{watermelon.textureScale}</td>
+            <td>{watermelon.rating}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
-
-export default Table
--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    SANITY_ID: process.env.SANITY_ID,
+  },
 }
 
 module.exports = nextConfig
